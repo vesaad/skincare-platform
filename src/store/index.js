59fcf6a562c287f457fbaf3cf22941b5e47ea007
Vesa Delibeqa <vdelibeqa@gmail.com>
2026-05-29 02:34:01 +0200

    feat: add redux-persist for auth and routine state

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,32 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
 import authReducer from "./slices/authSlice";
 import productsReducer from "./slices/productsSlice";
 import routineReducer from "./slices/routineSlice";
 import notificationReducer from "./slices/notificationSlice";
 
+const storage = {
+  getItem: (key) => Promise.resolve(localStorage.getItem(key)),
+  setItem: (key, value) => Promise.resolve(localStorage.setItem(key, value)),
+  removeItem: (key) => Promise.resolve(localStorage.removeItem(key)),
+};
+
+const routinePersistConfig = { key: "routine", storage };
+const authPersistConfig    = { key: "auth",    storage };
+
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
-    products: productsReducer,
-    routine: routineReducer,
+    auth:          persistReducer(authPersistConfig, authReducer),
+    products:      productsReducer,
+    routine:       persistReducer(routinePersistConfig, routineReducer),
     notifications: notificationReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
